a07f5c942224236af299892d75817a6ad0e5b880
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-03-31 17:22:36 +0200

    scrip leaflet in main js verschoben

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,20 @@
 /* Neuseelandreise Skript */
 // einzeiliger Kommentar
+
+var map = L.map('map').setView([-41.290, 174.006], 5);
+
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+}).addTo(map);
+
+
+
+L.marker([-41.290, 174.006]).addTo(map)
+
+    .bindPopup('Picton')
+
+    .openPopup();
