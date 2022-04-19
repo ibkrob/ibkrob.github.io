71bfb954559d2e5072c1d1e4b78f571ab0c898a2
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-04-19 12:08:38 +0200

    expo zur map hinzu

---
+++ routenverlauf/YouAreHere_2.js
@@ -0,0 +1,8 @@
+var json_YouAreHere_2 = {
+"type": "FeatureCollection",
+"name": "YouAreHere_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "You Are Here!" }, "geometry": { "type": "Point", "coordinates": [ 11.254734848906248, 47.456012141611389 ] } }
+]
+}
