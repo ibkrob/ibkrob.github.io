4e01367db3d0196221706ad22dac7288c6a7ca3e
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-04-29 10:55:59 +0200

    update map mit fullscreen minimap

--- nz/main.js
+++ nz/main.js
@@ -79,7 +79,53 @@ for (let hut of HUTS) {
     }
 
         L.circleMarker([hut.lat, hut.lng], {
-            color: statuscolor
+            color: statuscolor,
+            radius:50
         }).addTo(map).bindPopup(popup);
 
 };
+
+    // Baselayer 
+let startLayer = L.tileLayer.provider("OpenStreetMap.Mapnik");
+
+    // Weitere (weltweiten) Baselayer 
+let layerControl = L.control.layers(
+    {   "OSM Standard": startLayer,
+        "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
+        "Stadia Outdoors": L.tileLayer.provider("Stadia.Outdoors"),
+        "Thunderforest SpinalMap": L.tileLayer.provider("Thunderforest.SpinalMap"),
+        "Thunderforest Landscape": L.tileLayer.provider("Thunderforest.Landscape"),
+        "Stamen Watercolor": L.tileLayer.provider("Stamen.Watercolor"),
+        "Stamen Terrain": L.tileLayer.provider("Stamen.Terrain"),
+        "Stamen schwarzweiß": L.tileLayer.provider("Stamen.TonerLite"),
+        "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+        "Mtb Map": L.tileLayer.provider("MtbMap"),
+        "NASA Earth at Night": L.tileLayer.provider("NASAGIBS.ViirsEarthAtNight2012"),
+        "NASA Terra": L.tileLayer.provider("NASAGIBS.ModisTerraTrueColorCR"),
+        "NASA Terrabands": L.tileLayer.provider("NASAGIBS.ModisTerraBands367CR"),
+        "Stamen Watercolor mit Beschriftung": L.layerGroup([
+            L.tileLayer.provider("Stamen Watercolor"),
+            L.tileLayer.provider("Stamen.TonerHybrid")
+        ])
+        
+    }).addTo(map);
+
+    layerControl.expand()
+
+    let sightLayer=L.featureGroup();
+    layerControl.addOverlay(sightLayer, "Sehenswürdigkeiten");
+
+    let mrk= L.marker ([stephansdom.lat, stephansdom.lng]).addTo(sightLayer);
+
+    sightLayer.addTo(map);
+
+    // Maßstab hinzufügen
+    L.control.scale({
+        imperial:false
+    }).addTo(map);
+
+    L.control.fullscreen().addTo(map);
+
+    let miniMap= new L.Control.MiniMap(
+        L.tileLayer.provider("BasemapAT")
+    ).addTo(map);
